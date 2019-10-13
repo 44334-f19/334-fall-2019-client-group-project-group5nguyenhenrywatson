ee1cb481af54de0b421b39612f528f2f691ade39
zachariahwatson <54414185+zachariahwatson@users.noreply.github.com>
2019-10-12 19:47:06 -0500

    Added wrapper around header to remove need to add and remove margin-bottom in the JS

--- javascript testing/script.js
+++ javascript testing/script.js
@@ -2,10 +2,8 @@ $(window).scroll(function() {
     var logoheight = $("#logo").outerHeight();
     if (window.pageYOffset > document.getElementById("header").offsetTop) {
         $("#header").addClass("sticky");
-        $("#logo").css("margin-bottom", "150px");
         if (logoheight >= $(window).scrollTop()) {
             $("#header").removeClass("sticky");
-            $("#logo").css("margin-bottom", "0");
         }
     }
 });
