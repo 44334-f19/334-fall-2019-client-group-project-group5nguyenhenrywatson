7e56c5fb1078527d8ef0c85951c904f0401d55a2
zachariahwatson <54414185+zachariahwatson@users.noreply.github.com>
2019-10-14 10:44:50 -0500

    Wrote sticky header on scroll up function

--- javascript testing/script.js
+++ javascript testing/script.js
@@ -1,4 +1,5 @@
-$(window).scroll(function() {
+//sticky header
+/**$(window).scroll(function() {
     var logoheight = $("#logo").outerHeight();
     if (window.pageYOffset > document.getElementById("header").offsetTop) {
         $("#header").addClass("sticky");
@@ -6,4 +7,24 @@ $(window).scroll(function() {
             $("#header").removeClass("sticky");
         }
     }
+});**/
+
+//sticky header only on scroll up
+var temp = 0;
+$(window).scroll(function() {
+    var logoheight = $("#logo").outerHeight();
+    var scroll = $(window).scrollTop();
+    if ($(window).scrollTop() > document.getElementById("header").offsetTop + 300) {
+        $("#header").addClass("sticky");
+        $("#header").hide();
+        $(".sticky").css("top", "-200px");
+    }
+    if (temp > scroll) {
+        $("#header").show();
+        $(".sticky").css("top", "0px");
+        if (logoheight >= $(window).scrollTop()) {
+            $("#header").removeClass("sticky");
+        }
+    }
+    temp = $(window).scrollTop();
 });
