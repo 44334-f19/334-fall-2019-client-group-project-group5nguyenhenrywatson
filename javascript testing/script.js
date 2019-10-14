34a9d6f05de65b6b024b88640975ce412c2126c1
zachariahwatson <54414185+zachariahwatson@users.noreply.github.com>
2019-10-14 13:06:25 -0500

    Added comments to js function

--- javascript testing/script.js
+++ javascript testing/script.js
@@ -10,21 +10,21 @@
 });**/
 
 //sticky header only on scroll up
-var temp = 0;
+var temp = 0; //last scroll position
 $(window).scroll(function() {
     var logoheight = $("#logo").outerHeight();
     var scroll = $(window).scrollTop();
-    if ($(window).scrollTop() > document.getElementById("header").offsetTop + 300) {
+    if ($(window).scrollTop() > document.getElementById("header").offsetTop + 300) { //if the header is scrolled all the way past
         $("#header").addClass("sticky");
         $("#header").hide();
-        $(".sticky").css("top", "-200px");
+        $(".sticky").css("top", "-200px"); //hiding and preparing for css transition when shown(scroll up)
     }
-    if (temp > scroll) {
+    if (temp > scroll) { //if the current scroll position is less than the last
         $("#header").show();
-        $(".sticky").css("top", "0px");
-        if (logoheight >= $(window).scrollTop()) {
+        $(".sticky").css("top", "0px"); //show header (slide down transition)
+        if (logoheight >= $(window).scrollTop()) { //returning header back to its default position when you scroll to the top of the page
             $("#header").removeClass("sticky");
         }
     }
-    temp = $(window).scrollTop();
+    temp = $(window).scrollTop(); //updating last scroll position
 });
