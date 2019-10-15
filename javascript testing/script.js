03b93473a354db6053eab2362c9ccb2d330c0c53
zachariahwatson <54414185+zachariahwatson@users.noreply.github.com>
2019-10-15 10:45:31 -0500

    Added deadzone for sticky header

--- javascript testing/script.js
+++ javascript testing/script.js
@@ -20,17 +20,19 @@ $(window).scroll(function() {
         $("#header").hide();
         $(".sticky").css("top", "-200px"); //hiding and preparing for css transition when shown(scroll up)
     }
-    if (temp > scroll) { //if the last scroll position is greater than the current
+    if (temp  > scroll) { //if the last scroll position is greater than the current
         if (scrolled == false) { //if the header hasnt been displayed, display it
+            var deadzone = temp - 20; //added deadzone to stop header from showing up weirdly on mobile
             setTimeout(function() { //checking to see if user is still scrolling up after set amount of time
-                if (temp  >= scroll) {
+                if (deadzone > scroll) {
+                    console.log("show")
                     $("#header").show();
                     $(".sticky").css("top", "0px"); //show header (slide down transition)
                     if (logoheight >= $(window).scrollTop()) { //returning header back to its default position when you scroll to the top of the page
                         $("#header").removeClass("sticky");
                     }
+                    scrolled = true; //running this function once eliminate setTimeout firing multiple times which results in the header not showing
                 }
-                scrolled = true; //running this function once eliminate setTimeout firing multiple times which results in the header not showing
             }, 400);
         }
         if (scrolled == true) { //if the header has already been displayed keep displaying it
