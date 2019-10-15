0b783b5f7745544388cdd7bfb5b76526fe5586cf
zachariahwatson <54414185+zachariahwatson@users.noreply.github.com>
2019-10-15 11:02:40 -0500

    Removed delay portion of sticky header, added deadzone

--- javascript testing/script.js
+++ javascript testing/script.js
@@ -9,42 +9,28 @@
     }
 });**/
 
-//sticky header only on scroll up (with delay)
+//sticky header only on scroll up (with deadzone)
 var temp = 0; //last scroll position
 var scrolled = false;
 $(window).scroll(function() {
     var logoheight = $("#logo").outerHeight();
     var scroll = $(window).scrollTop();
-    if ($(window).scrollTop() > document.getElementById("header").offsetTop + 300) { //if the header is scrolled all the way past
+    if (($(window).scrollTop() > document.getElementById("header").offsetTop + 300) && (temp < scroll)) { //if the header is scrolled all the way past
         $("#header").addClass("sticky");
         $("#header").hide();
         $(".sticky").css("top", "-200px"); //hiding and preparing for css transition when shown(scroll up)
+        console.log("hide");
     }
-    if (temp  > scroll) { //if the last scroll position is greater than the current
-        if (scrolled == false) { //if the header hasnt been displayed, display it
-            var deadzone = temp - 20; //added deadzone to stop header from showing up weirdly on mobile
-            setTimeout(function() { //checking to see if user is still scrolling up after set amount of time
-                if (deadzone > scroll) {
-                    console.log("show")
-                    $("#header").show();
-                    $(".sticky").css("top", "0px"); //show header (slide down transition)
-                    if (logoheight >= $(window).scrollTop()) { //returning header back to its default position when you scroll to the top of the page
-                        $("#header").removeClass("sticky");
-                    }
-                    scrolled = true; //running this function once eliminate setTimeout firing multiple times which results in the header not showing
-                }
-            }, 400);
-        }
-        if (scrolled == true) { //if the header has already been displayed keep displaying it
+    if (temp > scroll) { //if the last scroll position is greater than the current
+        var deadzone = temp - 75; //added deadzone to requre the user to scroll at a certain speed for the header to show up
+        if (deadzone > scroll) {
+            console.log("show")
             $("#header").show();
             $(".sticky").css("top", "0px"); //show header (slide down transition)
-            if (logoheight >= $(window).scrollTop()) { //returning header back to its default position when you scroll to the top of the page
-                $("#header").removeClass("sticky");
-            }
         }
-    }
-    if (scroll > temp) { //resetting header show process if scroll down
-        scrolled = false;
+        if (logoheight >= $(window).scrollTop()) { //returning header back to its default position when you scroll to the top of the page
+            $("#header").removeClass("sticky");
+        }
     }
     temp = $(window).scrollTop(); //updating last scroll position
 });
