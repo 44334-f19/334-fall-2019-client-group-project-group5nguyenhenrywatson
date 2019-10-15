42959253e0f3a416d87f75dd67734566c6867948
zachariahwatson <54414185+zachariahwatson@users.noreply.github.com>
2019-10-15 10:25:23 -0500

    Updated the sticky header to allow for delay

--- javascript testing/script.js
+++ javascript testing/script.js
@@ -9,8 +9,9 @@
     }
 });**/
 
-//sticky header only on scroll up
+//sticky header only on scroll up (with delay)
 var temp = 0; //last scroll position
+var scrolled = false;
 $(window).scroll(function() {
     var logoheight = $("#logo").outerHeight();
     var scroll = $(window).scrollTop();
@@ -19,12 +20,29 @@ $(window).scroll(function() {
         $("#header").hide();
         $(".sticky").css("top", "-200px"); //hiding and preparing for css transition when shown(scroll up)
     }
-    if (temp > scroll) { //if the current scroll position is less than the last
-        $("#header").show();
-        $(".sticky").css("top", "0px"); //show header (slide down transition)
-        if (logoheight >= $(window).scrollTop()) { //returning header back to its default position when you scroll to the top of the page
-            $("#header").removeClass("sticky");
+    if (temp > scroll) { //if the last scroll position is greater than the current
+        if (scrolled == false) { //if the header hasnt been displayed, display it
+            setTimeout(function() { //checking to see if user is still scrolling up after set amount of time
+                if (temp  >= scroll) {
+                    $("#header").show();
+                    $(".sticky").css("top", "0px"); //show header (slide down transition)
+                    if (logoheight >= $(window).scrollTop()) { //returning header back to its default position when you scroll to the top of the page
+                        $("#header").removeClass("sticky");
+                    }
+                }
+                scrolled = true; //running this function once eliminate setTimeout firing multiple times which results in the header not showing
+            }, 400);
         }
+        if (scrolled == true) { //if the header has already been displayed keep displaying it
+            $("#header").show();
+            $(".sticky").css("top", "0px"); //show header (slide down transition)
+            if (logoheight >= $(window).scrollTop()) { //returning header back to its default position when you scroll to the top of the page
+                $("#header").removeClass("sticky");
+            }
+        }
+    }
+    if (scroll > temp) { //resetting header show process if scroll down
+        scrolled = false;
     }
     temp = $(window).scrollTop(); //updating last scroll position
 });
