032a91f12f733017906234be5578a256396b9bd2
zachariahwatson <54414185+zachariahwatson@users.noreply.github.com>
2019-11-07 13:12:52 -0600

    Fixed responsive nav displaying dropdown in mobile

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    var width = $(window).width();
     //sticky header only on scroll up (with deadzone)
     var temp = 0; //last scroll position
     $(window).scroll(function() {
@@ -39,20 +40,18 @@ $(document).ready(function() {
     });
     //js slide dropdown menu
     $(window).resize(function() {
-        if ($(window).width() > 1000) { //if the window is resized to above 1000
+        width = $(window).width();
+        if (width > 1000) { //if the window is resized to above 1000
             $("#navlinks").css("display", "flex"); //display the desktop version of nav
-            $("#aboutcontainer").hover(function() { //dropdown function w/ animation
-                $("#dropdown").slideDown(200);
-            }, function() {
-                $("#dropdown").slideUp(200);
-            });
         } else { //if window is resized to below 1000, hide menu and display mobile hamburger
             $("#navlinks").hide();
         }
     });
     //if user hasnt resized the window and theyre on desktop (normal dropdown function)
-    if ($(window).width() > 1000) {
+    if (width > 1000) {
         $("#aboutcontainer").hover(function() {
+            console.log($(window).width());
+            console.log(width);
             $("#dropdown").slideDown(200);
         }, function() {
             $("#dropdown").slideUp(200);
